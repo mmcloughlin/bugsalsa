563b3521f6433ff0c3a07dcf0774bb012f9c02de
Michael McLoughlin <mmcloughlin@gmail.com>
2019-03-02 14:25:15 +0000

    add logging to test binary

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <inttypes.h>
 #include <assert.h>
+#include <time.h>
 
 #include "crypto_stream_salsa20.h"
 
@@ -11,25 +12,36 @@
 #define COMPARE_SIZE (512ul)
 #define CIPHER_LENGTH (WRAP_POSITION + (4*COMPARE_SIZE))
 
+static void log_print(const char* message)
+{
+	time_t now;
+	time(&now);
+	char buf[sizeof "2011-10-08T07:07:09Z"];
+	strftime(buf, sizeof buf, "%FT%TZ", gmtime(&now));
+	fprintf(stderr, "%s\t%s\n", buf, message);
+}
+
 int main()
 {
+	log_print("start");
+
 	// Produce keystream of length CIPHER_LENGTH.
 	uint8_t nonce[crypto_stream_salsa20_NONCEBYTES] = {0};
 	uint8_t key[crypto_stream_salsa20_KEYBYTES] = {0};
 
 	uint8_t *buffer = (uint8_t *)calloc(CIPHER_LENGTH, sizeof(uint8_t));
 	assert(buffer != NULL);
-	fprintf(stderr, "buffer allocated\n");
+	log_print("buffer allocated");
 
 	int status = crypto_stream_salsa20_xor(buffer, buffer, CIPHER_LENGTH, nonce, key);
 	assert(status == 0);
-	fprintf(stderr, "keystream written\n");
+	log_print("keystream written");
 
 	// Dump it.
 	fwrite(buffer, CIPHER_LENGTH, sizeof(uint8_t), stdout);
-	fprintf(stderr, "output written\n");
+	log_print("output written");
 
 	// Cleanup.
 	free(buffer);
-	fprintf(stderr, "finish\n");
+	log_print("finish");
 }
